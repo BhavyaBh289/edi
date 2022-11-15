eb58d4f46bfbbd48c4f40ba6998a2f741ba8eaf3
BhavyaBh289 <shahbhavyasp@gmail.com>
2022-11-15 20:08:29 +0530

    .

--- IPC_finder/changes/panda.py
+++ IPC_finder/changes/panda.py
@@ -1,5 +1,13 @@
 import pandas as pd
+from rank_bm25 import BM25Okapi
 df = pd.read_csv("IPC_whole - IPC_whole.csv")
-print(df)
-questions = df["4"]
-print(questions[49])
+print(df["2"])
+inp = ["murder","riot"]
+ld = df["2"]
+for doc in ld:
+    print(doc)
+languagedata = [doc.split(" ") for doc in ld]
+bm25 = BM25Okapi(languagedata)
+doc_scores = bm25.get_scores(inp[0])
+for i in doc_scores:
+    print(i)
