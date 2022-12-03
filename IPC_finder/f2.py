63fec10f372daa52ca44bc0f978c6a148b205b83
BhavyaBh289 <shahbhavyasp@gmail.com>
2022-12-03 18:24:21 +0530

    new

--- IPC_finder/f2.py
+++ IPC_finder/f2.py
@@ -4,16 +4,16 @@ from rank_bm25 import BM25Okapi
 import tkinter
 #global variables declared till
 n=[]                    # contains keywords  ##after keywordextract is run ####keywordextract,bmitoquest
-df = pd.read_csv("IPC_whole - IPC_whole.csv")       #contains the database done  manually ##alltime ####bmitoquest,exit
+df = pd.read_csv("IPC_whole_final.csv")       #contains the database done  manually ##alltime ####bmitoquest,exit
 ips=[]          #contains all ipcs suspected in the code and passes them to ask questions  contains ipcs after  ##after bmitoquest ####bmitoquest, exit
 questionlist=[]                 # contains all the questions to be asked ## after bmitoquest ####bmitoquest, yes, no
 accepted_questions = []             #contains all ipcs which are sure to be applied ##after yes ## yes exit
 x=0         #number of ipc in list whose questions are being answered ##yes and no ####yes,no
 y=0         #number of questions are being answered ##yes and no ####yes,no
+sample = """ My name is Shirin Shaikh. i was admitted to a hospital. the nurse gave me wrong medicines due to which my health got even worse. i complained about this to the local police officer but the hospital authority bribed the police and my case was dismissed. """
 def keywordextract():
-    global n
+    global n , sample
     extractor = pke.unsupervised.TopicRank()
-    sample = """ Sir, my humble submission to you is that I am Roshan Bibi Swami Sikh Sabarak Darji, Village:-Shankra-Kh,Po:-Shankra,Thana:-Para,Dist:- Purulia resident.As per Muslim Neon last dated 05.09.2017 Married to Sekh Sabarak Darji, son of Sekh Jugnu Darji of village Shankra-Kh under Para police station.Currently, I am the mother of a 03-year-old son, from the last one year, my husband and in-laws have been abusing me mentally and physically in various ways. They also beat my three-year-old child by not letting him eat or verbally beating him. I have tried to survive in the world despite all the tortures. After paying all the dowry during the marriage, the torture on me increased for the last nine-ten months, and I was repeatedly told by them to buy my father a Bullet car. Then my father and brother came to convince my in-laws and insulted them and sent me and my child to my father's house on 29-09-2021. While leaving, my brother-in-law and mother-in-law told me that the Hero Glamor whose registration number JH09AG 6215 was given as dowry during the marriage should be replaced by a new BULLET car, otherwise they should come back or kill their son i.e. me. Husband will remarry. Even though I want to return to in-laws house again and again, my husband or in-laws keep humiliating me and demanding a new BULLET car. is doing Now I know for sure that my husband is remarrieddid So sir my husband 1) Sikh Sabarak Darji, 2) Vasur Sikh Tabarak Darji, 3) Nand-Parbina Bibi, 4) Sikh Jaganu Darji father-deceased Nishad Darji and 5) Mother-in-law Khairuna Bibi, husband- Jaganu Darji legal action against all of them. Take appropriate measures to punish the maidservants and take necessary steps to secure the future life of me and my children. My complaint was delayed because I was hoping it would take me now that my husband had remarried."""
     extractor.load_document(input=sample, language='en')
     a=[]
     n=[]
@@ -24,7 +24,18 @@ def keywordextract():
         for c in i:
             for d in c:
                 n.append(d)
-    # print(n)
+    print(n, i)
+def keyword_changing():
+    #keyword adding and removing
+    global n , sample
+    verb =[]
+    nouns=[]
+    for i in n :
+        if i in verb :
+            n.remove(i)
+    for i in nouns:
+        if i in sample:
+            n.append(i)
 
 def bmitoquest():
     global questionlist,ips,df,n
@@ -34,19 +45,21 @@ def bmitoquest():
     doc_scores = bm25.get_scores(n)
     questionlist = []
     ips = []
+    tp = []
     t=-2
     tempstr="test"
     for i in doc_scores:
         t+=1
         if i > 0.1:
             ips.append(t)
+            tp.append(df["1"][t])
             temp=[df["4"][t],df["5"][t],df["6"][t]]
             temp2=[]
             for temp3 in temp:
                 if type(temp3)==type(tempstr):
                     temp2.append(temp3)
             questionlist.append(temp2)
-    # print(questionlist,ips)
+    print(questionlist,tp)
 
 keywordextract()
 bmitoquest()
@@ -58,7 +71,6 @@ def exitt():
     for i in accepted_questions :
         print(df["1"][ips[i]])
 def yes():
-
     global x,y,accepted_questions
     # print ("y",x,y,len(questionlist[x]),len(questionlist))
     if y <len(questionlist[x])-1:
---
+++ IPC_finder/t1.py
@@ -0,0 +1,102 @@
+import pke
+import pandas as pd
+from rank_bm25 import BM25Okapi
+import tkinter
+#global variables declared till
+n=[]                    # contains keywords  ##after keywordextract is run ####keywordextract,bmitoquest
+df = pd.read_csv("IPC_whole_final.csv")       #contains the database done  manually ##alltime ####bmitoquest,exit
+ips=[]          #contains all ipcs suspected in the code and passes them to ask questions  contains ipcs after  ##after bmitoquest ####bmitoquest, exit
+questionlist=[]                 # contains all the questions to be asked ## after bmitoquest ####bmitoquest, yes, no
+accepted_questions = []             #contains all ipcs which are sure to be applied ##after yes ## yes exit
+x=0         #number of ipc in list whose questions are being answered ##yes and no ####yes,no
+y=0         #number of questions are being answered ##yes and no ####yes,no
+def keywordextract():
+    global n
+    extractor = pke.unsupervised.TopicRank()
+    sample = """Greeting sir i am a farmer and i have been on this land for the past 13 years. a local vendor and i were abused by the politician of this area and they been take ransome amount from us without us having any benefit. please take this into account."""
+    extractor.load_document(input=sample, language='en')
+    a=[]
+    n=[]
+    extractor.candidate_selection(pos={'VERB'})
+    for i, candidate in enumerate(extractor.candidates):
+        a.append( u for u in extractor.candidates[candidate].surface_forms)
+    for i in a:
+        for c in i:
+            for d in c:
+                n.append(d)
+    print(n)
+
+def bmitoquest():
+    global questionlist,ips,df,n
+    ld = df["3"]
+    languagedata = [doc.split(" ") for doc in ld]
+    bm25 = BM25Okapi(languagedata)
+    doc_scores = bm25.get_scores(n)
+    questionlist = []
+    ips = []
+    ipclist=[]
+    t=-2
+    tempstr="test"
+    for i in doc_scores:
+        t+=1
+        if i > 1 and i < 2:
+            ipclist.append(df["1"][t])
+            ips.append(t)
+            temp=[df["4"][t],df["5"][t],df["6"][t]]
+            temp2=[]
+            for temp3 in temp:
+                if type(temp3)==type(tempstr):
+                    temp2.append(temp3)
+            questionlist.append(temp2)
+            print(ld[t],i)
+    print(ipclist)
+    # print(questionlist,ips)
+
+keywordextract()
+bmitoquest()
+
+
+
+# def exitt():
+#     global accepted_questions,ips,df
+#     for i in accepted_questions :
+#         print(df["1"][ips[i]])
+# def yes():
+#
+#     global x,y,accepted_questions
+#     # print ("y",x,y,len(questionlist[x]),len(questionlist))
+#     if y <len(questionlist[x])-1:
+#         y+=1
+#     elif y == len(questionlist[x])-1:
+#         y = 0
+#         accepted_questions.append(x)
+#         if x == len(questionlist)-1:
+#             exitt()
+#             root.destroy()
+#             return
+#         x+=1
+#     questions.configure(text =questionlist[x][y])
+# def no():
+#     global x,y
+#     # print ("n",x,y,len(questionlist[x]),len(questionlist))
+#     y = 0
+#     if x == len(questionlist)-1:
+#         exitt()
+#         root.destroy()
+#         return
+#     x+=1
+#     questions.configure(text =questionlist[x][y])
+# root = tkinter.Tk()
+# root.geometry("700x300")
+# root.title("questions")
+#
+# questions = tkinter.Label(root, font=("arial", 25), text=questionlist[0][0])
+# questions.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
+# yes_btn = tkinter.Button(root,font=("Times New Roman", 25),text = "Yes", command = yes)
+# yes_btn.grid(row=5, column=0, columnspan=1, padx=5, pady=5)
+# maybe_btn = tkinter.Button(root,font=("Times New Roman", 25),text = "Maybe", command = yes)
+# maybe_btn.grid(row=5, column=2, columnspan=1, padx=5, pady=5)
+# no_btn = tkinter.Button(root,font=("Times New Roman", 25),text = "No",command = no)
+# no_btn.grid(row=5, column=5, columnspan=1, padx=5, pady=5)
+# root.mainloop()
+
